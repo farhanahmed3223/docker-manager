5062dd22d20173001d8aaebc455999d7c3eaf43b
farhanahmed3223 <farhanahmed3223@proton.me>
2021-11-21 01:11:00 +0500

    wip tui with bubbletea, just renders text

---
+++ internal/ui/model.go
@@ -0,0 +1,35 @@
+package ui
+
+import (
+	"docker-manager/internal/docker"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	containers []docker.ContainerInfo
+	cursor     int
+	err        error
+}
+
+func NewModel() Model {
+	return Model{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	return "docker manager tui - wip\n\nPress q to quit."
+}
