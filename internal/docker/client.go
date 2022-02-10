f1fa2b08aa0accdeca076782b7ad57a2e166f3d0
farhanahmed3223 <farhanahmed3223@proton.me>
2022-02-10 19:22:00 +0500

    fix: cpu stats parsing was wrong formula

--- internal/docker/client.go
+++ internal/docker/client.go
@@ -2,7 +2,9 @@ package docker
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -17,6 +19,17 @@ type ContainerInfo struct {
 	Ports   string
 	State   string
 	Created int64
+	CPUPerc float64
+	MemPerc float64
+}
+
+type StatsResult struct {
+	CPUPercent float64
+	MemUsage   uint64
+	MemLimit   uint64
+	MemPercent float64
+	NetRx      uint64
+	NetTx      uint64
 }
 
 type DockerClient struct {
@@ -32,6 +45,9 @@ func NewDockerClient() (*DockerClient, error) {
 }
 
 func (d *DockerClient) ListContainers(ctx context.Context) ([]ContainerInfo, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	raw, err := d.cli.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		return nil, err
@@ -52,4 +68,53 @@ func (d *DockerClient) ListContainers(ctx context.Context) ([]ContainerInfo, err
 		})
 	}
 	return out, nil
+}
+
+func (d *DockerClient) GetStats(ctx context.Context, nameOrID string) (*StatsResult, error) {
+	resp, err := d.cli.ContainerStats(ctx, nameOrID, false)
+	if err != nil {
+		return nil, fmt.Errorf("stats error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var raw types.StatsJSON
+	if err := json.Unmarshal(body, &raw); err != nil {
+		return nil, fmt.Errorf("failed to parse stats: %w", err)
+	}
+
+	// CPU % = (cpu_delta / system_delta) * num_cpus * 100
+	cpuDelta := float64(raw.CPUStats.CPUUsage.TotalUsage - raw.PreCPUStats.CPUUsage.TotalUsage)
+	sysDelta := float64(raw.CPUStats.SystemUsage - raw.PreCPUStats.SystemUsage)
+	numCPU := float64(len(raw.CPUStats.CPUUsage.PercpuUsage))
+	cpuPct := 0.0
+	if sysDelta > 0 && cpuDelta > 0 {
+		cpuPct = (cpuDelta / sysDelta) * numCPU * 100.0
+	}
+
+	memUsage := raw.MemoryStats.Usage
+	memLimit := raw.MemoryStats.Limit
+	memPct := 0.0
+	if memLimit > 0 {
+		memPct = float64(memUsage) / float64(memLimit) * 100.0
+	}
+
+	var netRx, netTx uint64
+	for _, v := range raw.Networks {
+		netRx += v.RxBytes
+		netTx += v.TxBytes
+	}
+
+	return &StatsResult{
+		CPUPercent: cpuPct,
+		MemUsage:   memUsage,
+		MemLimit:   memLimit,
+		MemPercent: memPct,
+		NetRx:      netRx,
+		NetTx:      netTx,
+	}, nil
 }
